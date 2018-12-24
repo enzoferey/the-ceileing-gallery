11730e746316eb9afa18cbf722f903fc548bd2bb
Enzo Ferey <hello@enzoferey.com>
2018-12-24 20:23:22 +0100

    Full width header.

--- src/components/header.js
+++ src/components/header.js
@@ -8,51 +8,50 @@ const Header = ({ siteTitle }) => (
   <div
     style={{
       background: `white`,
-      marginBottom: `1.45rem`,
-      margin: `0 auto`,
-      maxWidth: 960,
     }}
   >
-    <div className="header-title">
-      <h1>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <nav className="header-nav">
-        <Link
-          to="/"
-          style={{
-            flex: 1,
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            color: 'black',
-            marginRight: '20px',
-            textDecoration: 'none',
-          }}
-        >
-          Purchase
-        </Link>
-        <Link
-          to="/"
-          style={{
-            flex: 1,
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            color: 'black',
-            marginLeft: '20px',
-            textDecoration: 'none',
-          }}
-        >
-          About the artist
-        </Link>
-      </nav>
+    <div style={{ marginBottom: `1.45rem`, margin: `0 auto`, maxWidth: 960 }}>
+      <div className="header-title">
+        <h1>
+          <Link
+            to="/"
+            style={{
+              color: `black`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <nav className="header-nav">
+          <Link
+            to="/"
+            style={{
+              flex: 1,
+              textTransform: 'uppercase',
+              fontWeight: 'bold',
+              color: 'black',
+              marginRight: '20px',
+              textDecoration: 'none',
+            }}
+          >
+            Purchase
+          </Link>
+          <Link
+            to="/"
+            style={{
+              flex: 1,
+              textTransform: 'uppercase',
+              fontWeight: 'bold',
+              color: 'black',
+              marginLeft: '20px',
+              textDecoration: 'none',
+            }}
+          >
+            About the artist
+          </Link>
+        </nav>
+      </div>
     </div>
   </div>
 )
